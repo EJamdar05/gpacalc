ef1b71e2503497f46709fb992203e210459940ef
EJamdar05 <jamdareshaq@gmail.com>
2021-02-15 18:02:54 -0800

    commit first change

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+let submittedData = document.getElementById('generate');
+submittedData.addEventListener("click", calculate);
+function calculate(event){
+    let grades = document.getElementById('text-field').value;
+    let line = grades.split('\n');
+    let letterGrades = [];
+    let courseHours = [];
+    for(let i = 0; i < line.length;i++){
+        let splitGrade = line[i].split(' ');
+        letterGrades[i] = splitGrade[1];
+        if(splitGrade[2]!=""){
+            courseHours[i] = parseInt(splitGrade[2]);
+        }
+    }
+    let grade;
+    for(let j = 0 ; j < letterGrades.length;j++){
+        grade = letterGrades[j];
+        if(grade == 'A' || grade == 'a'){
+            letterGrades[j] = 4;
+        }
+        else if(grade == 'A-' || grade == 'a-'){
+            letterGrades[j] = 3.67;
+        }
+        else if(grade == 'B+' || grade == 'b+'){
+            letterGrades[j] = 3.33;
+        }
+        else if(grade == 'B' || grade == 'b'){
+            letterGrades[j] = 3;
+        }
+        else if(grade == 'C+' || grade == 'c+'){
+            letterGrades[j] = 2.33;
+        }
+        else if(grade == 'C' || grade == 'c'){
+            letterGrades[j] = 2;
+        }
+        else if(grade == 'D' || grade == 'd'){
+            letterGrades[j] = 1;
+        }
+        else{
+            letterGrades[j] = 0;
+        }
+     }
+     let gpa = 0;
+     let hours = 0;
+     for (let i = 0; i < letterGrades.length;i++){
+         gpa+=letterGrades[i]*courseHours[i];
+     }
+     for (let i = 0; i < courseHours.length;i++){
+        hours+=courseHours[i];
+    }
+    
+
+}
